c4f5e9d1a84ffec652bbd2ea2b8cb04924f78769
Juri Krainjukov <jurik@proekspert.ee>
2018-08-23 00:35:38 +0300

    config db

--- services/users/manage.py
+++ services/users/manage.py
@@ -3,13 +3,18 @@ import os
 
 from flask.cli import FlaskGroup
 
-from project import app
+from project import app, db
 
 
 cli = FlaskGroup(app)
 
 
+@cli.command()
+def recreate_db():
+    db.drop_all()
+    db.create_all()
+    db.session.commit()
+
+
 if __name__ == '__main__':
-    # os.environ['FLASK_APP'] = 'project/__init__.py'
-    # os.environ['FLASK_ENV'] = 'development'
     cli()
--- services/users/project/__init__.py
+++ services/users/project/__init__.py
@@ -1,7 +1,6 @@
-# services/users/project/__init__.py
-
 import os
 from flask import Flask, jsonify
+from flask_sqlalchemy import SQLAlchemy  # new
 
 
 # instantiate the app
@@ -11,10 +10,24 @@ app = Flask(__name__)
 app_settings = os.getenv('APP_SETTINGS')
 app.config.from_object(app_settings)
 
-#import sys
-#print(app.config, file=sys.stderr)
+# instantiate the db
+db = SQLAlchemy(app)  # new
 
 
+# model
+class User(db.Model):  # new
+    __tablename__ = 'users'
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    username = db.Column(db.String(128), nullable=False)
+    email = db.Column(db.String(128), nullable=False)
+    active = db.Column(db.Boolean(), default=True, nullable=False)
+
+    def __init__(self, username, email):
+        self.username = username
+        self.email = email
+
+
+# routes
 @app.route('/users/ping', methods=['GET'])
 def ping_pong():
     return jsonify({
