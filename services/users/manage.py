56449e29179278c2df4341e9519eae0b1b50c8f9
Juri Krainjukov <jurik@proekspert.ee>
2018-08-22 14:54:48 +0300

    start

---
+++ services/users/manage.py
@@ -0,0 +1,15 @@
+# services/users/manage.py
+import os
+
+from flask.cli import FlaskGroup
+
+from project import app
+
+
+cli = FlaskGroup(app)
+
+
+if __name__ == '__main__':
+    os.environ['FLASK_APP'] = 'project/__init__.py'
+    os.environ['FLASK_ENV'] = 'development'
+    cli()
