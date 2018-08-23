348f2be6c40a389e411c5b0dc3b0ef8d37dfb367
Juri Krainjukov <jurik@proekspert.ee>
2018-08-23 11:28:40 +0300

    add user form

--- services/client/src/App.js
+++ services/client/src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from "react";
 import axios from "axios";
 import UsersList from "./components/UsersList";
+import AddUser from "./components/AddUser";
 
 class App extends Component {
   state = {
@@ -21,16 +22,26 @@ class App extends Component {
       });
   }
 
+  addUser = event => {
+    event.preventDefault();
+    console.log("sanity check!");
+  };
+
   render() {
     return (
       <section className="section">
         <div className="container">
           <div className="columns">
-            <div className="column is-one-third">
+            <div className="column is-half">
+              {" "}
+              {/* new */}
               <br />
               <h1 className="title is-1">All Users</h1>
               <hr />
               <br />
+              <AddUser addUser={this.addUser} />
+              <br />
+              <br /> {/* new */}
               <UsersList users={this.state.users} />
             </div>
           </div>
