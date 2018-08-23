c7293d9c74954bd33490d3bd7df484bd67899435
Juri Krainjukov <jurik@proekspert.ee>
2018-08-23 04:32:19 +0300

    debug toolbar

--- services/users/project/config.py
+++ services/users/project/config.py
@@ -1,7 +1,4 @@
-# services/users/project/config.py
-
-
-import os  # new
+import os
 
 
 class BaseConfig:
@@ -9,11 +6,14 @@ class BaseConfig:
     TESTING = False
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     SECRET_KEY = 'my_precious'
+    DEBUG_TB_ENABLED = False
+    DEBUG_TB_INTERCEPT_REDIRECTS = False
 
 
 class DevelopmentConfig(BaseConfig):
     """Development configuration"""
     SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL')  # new
+    DEBUG_TB_ENABLED = True
 
 
 class TestingConfig(BaseConfig):
